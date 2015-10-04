19694fff944cc3d3696c9cfcbc5928d280e72df2
Juan Miret <juanmiret55@gmail.com>
2015-10-03 22:04:53 -0300

    Update init.el
    Changed init-ido to init-helm

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
 (require 'init-flycheck)
 
 (require 'init-recentf)
-(require 'init-ido)
+(require 'init-helm)
 (require 'init-hippie-expand)
 (require 'init-auto-complete)
 (require 'init-windows)
